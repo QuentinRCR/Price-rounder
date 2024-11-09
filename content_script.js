65f9575640b70c177ba8c185c65d923fb8d78998
Quentin REY <quentin.rey@etu.emse.fr>
2024-11-09 18:39:58 +0100

    improve the processing to generalize it

--- content_script.js
+++ content_script.js
@@ -1,93 +1,4 @@
-// doesn't work on manomano, bonlanger, ebay
-
-const processElements = (elements) => {
-    const valuePattern = `(\\d\\s*?){1,6}([.,]\\d{1,2})?`; //matches 22.90, 22.9, 22.9, 22, 22, 1000, 100 000, 100 000.02
-    const currencySymbolsRegex = `([€$£¥₹₩₽₺₪₦৳₱₨৳]|zł|CHF|د.إ|USD|EUR)`; // match any possible currency
-    const pricePattern = new RegExp(`(${valuePattern}\\s?${currencySymbolsRegex})|(${currencySymbolsRegex}\\s?${valuePattern})`,'g');
-
-    // const pricePattern = /\s?[€$£¥]/g; //matches 22.90 €, 22.9 €, 22.9€, 22€, 22 € 1000€ 100 000€ 100 000.02€
-
-    elements.forEach(element => {
-        // For elements without children
-        if (element.children.length==0 && element.textContent.match(pricePattern)) {
-
-            let prices = element.textContent.match(pricePattern);
-
-
-            prices.forEach(price => {
-                console.log("1-",price)
-                const currency_match = price.match(currencySymbolsRegex)
-                const currency = currency_match[0]
-                const currency_first = currency_match.index==0 //if the currency is a the beginning, it index is zero
-
-                const useComma = price.includes(','); // if the price is written with a comma
-                let originalPrice = price.replace(currency, '').replace(/\s+/g, ''); // Remove the currency and all white spaces
-
-                if (useComma){
-                    originalPrice = originalPrice.replace(',', '.');  //replace , with .
-                }  
-
-                let roundedPrice = roundPrice(Number(originalPrice)); // Round the price
-
-                let textPrice  = roundedPrice.toFixed(2); //add two decimals
-
-                if (useComma){
-                    textPrice = textPrice.replace(".", ",")
-                }
-
-                if(currency_first){
-                    textPrice = currency+textPrice;
-                }
-                else{
-                    textPrice += currency; // add the currency symbol back
-                }
-
-                // Replace the original price in the element's text content
-                element.textContent = element.textContent.replace(price, textPrice);
-            });
-        }
-        // If the element has child elements, we need a more complex approach
-        else if (element.classList.contains('product-price__price')) { //a bit specific for darty
-            // Find the price text
-            let priceTexts = element.firstChild.data.match(pricePattern);
-            
-            priceTexts.forEach((priceText)=>{
-                console.log("2-",priceText)
-                const currency_match = price.match(currencySymbolsRegex)
-                const currency = currency_match[0]
-                const currency_first = currency_match.index==0 //if the currency is a the beginning, it index is zero
-
-                let originalPrice = priceText.replace(currency, '').replace(',', '.').replace(/\s+/g, '');
-
-                let roundedPrice = roundPrice(Number(originalPrice)); // Round the price
-
-                let returnPrice  = roundedPrice.toFixed(2).replace(".", ","); //add two decimals
-
-                if(currency_first){
-                    returnPrice = currency+returnPrice;
-                }
-                else{
-                    returnPrice += currency; // add the currency symbol back
-                }
-
-                // Replace the original price in the element's text content
-                element.firstChild.data = element.firstChild.data.replace(priceText, returnPrice);
-            })
-        }
-
-        // handle amazon weird price display
-        else if(element.classList.contains('a-price')){
-            let priceContainer = element.querySelector('span[aria-hidden="true"]');
-            if (priceContainer.children.length > 0){ // ignore simple prices that were already handled by the first case            
-                //reconstruct the price from amazon weird price display
-                let priceText = (priceContainer.getElementsByClassName('a-price-whole')[0].textContent.replace(/\s+/g, '') +
-                                return_amazon_fraction(priceContainer)).replace(",",".").replace('..','.');
-                let roundedPrice = roundPrice(Number(priceText)); // Round the price
-                priceContainer.innerHTML = roundedPrice.toFixed(2).replace(".", ",") + '€' // replace the complicated spans with some texts
-            }
-        }
-    });
-}
+// doesn't work on manomano
 
 // add a fraction to the price if it exists
 function return_amazon_fraction(priceContainer){
@@ -97,6 +8,7 @@ function return_amazon_fraction(priceContainer){
      : "")
 }
 
+// round the price following some rules 
 function roundPrice(price) {
     // For prices over 200, round to the nearest ten if the units are more that 4.9 (295€ => 300€)
     if (price > 200 && price % 10 > 4.9) {
@@ -121,6 +33,70 @@ function roundPrice(price) {
     return price;
 }
 
+// Helper function to format and replace a price string
+function formatPrice(price, currencySymbolsRegex) {
+    const currencyMatch = price.match(currencySymbolsRegex);
+    const currency = currencyMatch[0];
+    const currencyFirst = currencyMatch.index === 0; //if the currency is a the beginning, it index is zero
+
+    let formattedPrice = price.replace(currency, '').replace(/\s+/g, ''); // remove the currency to round the price up
+    const useComma = formattedPrice.includes(',');
+
+    useComma && (formattedPrice = formattedPrice.replace(',', '.')); // Replace comma with period for parsing
+
+    let roundedPrice = roundPrice(Number(formattedPrice)).toFixed(2); //round price and convert back to 2 decimal
+
+    useComma && (roundedPrice = roundedPrice.replace('.', ',')); // Revert period to comma if original had comma
+
+    return currencyFirst ? currency + roundedPrice : roundedPrice + currency;
+}
+
+const processElements = (elements) => {
+    const valuePattern = `(\\d\\s*?){1,6}([.,]\\d{1,2})?`; //matches 22.90, 22.9, 22.9, 22, 22, 1000, 100 000, 100 000.02
+    const currencySymbolsRegex = `([€$£¥₹₩₽₺₪₦৳₱₨৳]|zł|CHF|د.إ|USD|EUR)`; // match any possible currency
+    const pricePattern = new RegExp(`(${valuePattern}\\s?${currencySymbolsRegex})|(${currencySymbolsRegex}\\s?${valuePattern})`,'g');
+
+    // const pricePattern = /\s?[€$£¥]/g; //matches 22.90 €, 22.9 €, 22.9€, 22€, 22 € 1000€ 100 000€ 100 000.02€
+
+    elements.forEach(element => {
+        //regular price
+        let target_node = null;
+        if(element.children.length <= 1){ // For elements close to the root
+            target_node = Array.from(element.childNodes).find(node => node.nodeType === Node.TEXT_NODE); //get the content written in the element
+        }
+        const matchedPrices = target_node?.nodeValue.match(pricePattern); // try to match a price
+        if (matchedPrices) {
+            matchedPrices?.forEach(price => {
+                const formattedPrice = formatPrice(price, currencySymbolsRegex); //format it
+                target_node.nodeValue = target_node.nodeValue.replace(price, formattedPrice);
+            });
+        }
+
+        // handle amazon weird price display
+        else if(element.classList.contains('a-price')){
+            let priceContainer = element.querySelector('span[aria-hidden="true"]');
+            if (priceContainer.children.length > 0){ // ignore simple prices that were already handled by the first case  
+                const priceText = element.getElementsByClassName('a-offscreen')[0].textContent
+                let formattedPrice;
+                if(priceText.match(pricePattern)){ // use the off screen price when it exist
+                    formattedPrice = formatPrice(priceText, currencySymbolsRegex);
+                }
+                else{ //otherwise reconstruct the price
+                    let priceText = (priceContainer.getElementsByClassName('a-price-whole')[0].textContent.replace(/\s+/g, '') +
+                                    return_amazon_fraction(priceContainer)).replaceAll(",",".").replace('..','.') + priceContainer.getElementsByClassName('a-price-symbol')[0].textContent;
+                    formattedPrice = formatPrice(priceText, currencySymbolsRegex);
+                }
+                priceContainer.innerHTML = formattedPrice;
+                
+                //reconstruct the price from amazon weird price display
+
+                // let roundedPrice = roundPrice(Number(priceText)); // Round the price
+                // priceContainer.innerHTML = roundedPrice.toFixed(2).replace(".", ",") + '€' // replace the complicated spans with some texts
+            }
+        }
+    });
+}
+
 
 
 function get_active_tab_hostname(){
@@ -140,7 +116,7 @@ async function initialize(force){
         console.log("The rounding has been initialized")
 
         // Initialization
-        initialRounding = () => processElements(document.querySelectorAll("body *"));
+        initialRounding = () => processElements(queryAllDeep("*"));
 
         if (document.readyState !== 'loading') {
             // document is already ready, just execute code here
@@ -175,7 +151,7 @@ function handleNewNodes(mutations) {
                 addedElements.push(node); // Add the element itself
                 
                 // Add all subtree elements as well
-                node.querySelectorAll('*').forEach(subNode => {
+                queryAllDeep('*',node).forEach(subNode => {
                     addedElements.push(subNode);
                 });
             }
@@ -206,3 +182,17 @@ function handleMessage(request, sender, sendResponse) {
 }
 
 browser.runtime.onMessage.addListener(handleMessage);
+
+// query all elements, including shadow ones
+function queryAllDeep(selector, root = document) {
+    const elements = Array.from(root.querySelectorAll(selector));
+
+    // Recursively search for the selector in each shadow root
+    root.querySelectorAll("*").forEach((element) => {
+        if (element.shadowRoot) {
+            elements.push(...queryAllDeep(selector, element.shadowRoot));
+        }
+    });
+
+    return elements;
+}
